29bb8446b2c72274a8e4e1ca3b5bf0807c334310
Alvaro Guzmán <alvaroguzmandg@gmail.com>
2023-01-30 09:22:11 -0300

    Ajuste links del menu

--- js/menu.js
+++ js/menu.js
@@ -15,10 +15,10 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="/profesionales">Profesionales</a>
+          <a class="nav-link" href="../profesionales">Profesionales</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/servicios">Servicios</a>
+          <a class="nav-link" href="../servicios">Servicios</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Clientes</a>
