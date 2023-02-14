fc204d4630e063638d0f943e3608967c16d0a039
Alvaro Guzmán <alvaroguzmandg@gmail.com>
2023-02-14 13:34:21 -0300

    .

--- js/publicacionesDestacadas.js
+++ js/publicacionesDestacadas.js
@@ -13,10 +13,9 @@
   </span>
 
   <span class="contenedorNota-bloque" data-aos="fade-up" data-aos-duration="1500">
-    <span class="nota-titulo">BILLETERAS DIGITALES: NUEVA REGULACIÓN
-      DEL BCRA PARA PREVENIR LOS FRAUDES</span>
-    <span class="nota-anticipo">El pasado 24 de febrero el Banco Central de la República Argentina (BCRA) emitió la Com. A 7463 en la que establece un conjunto de reglas para fortalecer el nivel... </span>
-    <button type="button" onclick="location.href='https://alvaroguzmandg.github.io/fidesNet/notas/billeteras-digitales-nueva-regulación-del-bcra-para-prevenir-los-fraudes.html'" class="nota-seguirLeyendo"> >seguir Leyendo</button>
+    <span class="nota-titulo">¿SE DEBERÍA FORMALIZAR UN PLAN DE IMPLEMENTACIÓN DE LA RESOLUCIÓN UIF Nº 112/2021?</span>
+    <span class="nota-anticipo">En una nota anterior de mi autoría, publicada con fecha 28.10.2021*, hacía un análisis respecto de los principales aspectos derivados de la emisión... </span>
+    <button type="button" onclick="location.href='https://alvaroguzmandg.github.io/fidesNet/notas/se-deberia-formalizar-un-plan-de-implementacion-de-la-resolucion-uif-no-112-2021.html'" class="nota-seguirLeyendo"> >seguir Leyendo</button>
   </span>
 </span>`
 
