2567556fbab868b51da58b9a0ca974507c79c4ff
Alvaro Guzmán <alvaroguzmandg@gmail.com>
2023-02-14 15:01:16 -0300

    .

--- js/contacto.js
+++ js/contacto.js
@@ -9,7 +9,10 @@
     <span class="titulo">Dejanos tu mensaje</span>
       <span class="contenidoForm w90 mw900 centrado fontTexto">
         <div class="mb-3">
-          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Correo electrónico">
+        <form method="GET" action="https://app.embluemail.com/Online/TriggerUrlExterna.aspx" onsubmit="return validateForm();" novalidate="">
+        <input type="hidden" name="key" value="ccc44137-0b8c-4849-be62-1a67f4c64e11">
+          <input type="email" class="form-control" id="email" name="email" required="" data-error="Campo requerido|Formato inválido" placeholder="Correo electrónico">
+
         </div>
         <div class="mb-3">
           <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Dejanos tu mensaje"></textarea>
@@ -19,7 +22,8 @@
             <input type="checkbox" name="recibir" id="suscribir" value="Quiero suscribirme al newsletter">
             <label>Quiero suscribirme al newsletter</label>
           </span>
-          <button type="submit">ENVIAR</button>
+          <button type="submit" type="submit" value="">ENVIAR</button>
+          </form>
         </span>
       </span>
   </span>
