a293b346689ef36090868b8b89607ba19f840473
Alvaro Guzmán <alvaroguzmandg@gmail.com>
2023-02-14 13:07:48 -0300

    NOTA

--- js/publicacionesDestacadas.js
+++ js/publicacionesDestacadas.js
@@ -9,14 +9,14 @@
     <span class="nota-titulo">BILLETERAS DIGITALES: NUEVA REGULACIÓN
       DEL BCRA PARA PREVENIR LOS FRAUDES</span>
     <span class="nota-anticipo">El pasado 24 de febrero el Banco Central de la República Argentina (BCRA) emitió la Com. A 7463 en la que establece un conjunto de reglas para fortalecer el nivel... </span>
-    <button type="button" onclick="location.href='../notas/template.html'" class="nota-seguirLeyendo"> >seguir Leyendo</button>
+    <button type="button" onclick="location.href='https://alvaroguzmandg.github.io/fidesNet/notas/billeteras-digitales-nueva-regulación-del-bcra-para-prevenir-los-fraudes.html'" class="nota-seguirLeyendo"> >seguir Leyendo</button>
   </span>
 
   <span class="contenedorNota-bloque" data-aos="fade-up" data-aos-duration="1500">
     <span class="nota-titulo">BILLETERAS DIGITALES: NUEVA REGULACIÓN
       DEL BCRA PARA PREVENIR LOS FRAUDES</span>
     <span class="nota-anticipo">El pasado 24 de febrero el Banco Central de la República Argentina (BCRA) emitió la Com. A 7463 en la que establece un conjunto de reglas para fortalecer el nivel... </span>
-    <button type="button" onclick="location.href='../notas/template.html'" class="nota-seguirLeyendo"> > seguir leyendo</button>
+    <button type="button" onclick="location.href='https://alvaroguzmandg.github.io/fidesNet/notas/billeteras-digitales-nueva-regulación-del-bcra-para-prevenir-los-fraudes.html'" class="nota-seguirLeyendo"> >seguir Leyendo</button>
   </span>
 </span>`
 
